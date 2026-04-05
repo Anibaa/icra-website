f3353e58d7e632a8c3ed1a48ff03c9c010ee5bee
Yassine Aniba <anibayassine@ieee.org>
2026-04-05 22:19:32 +0100

    assets sm logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/logos/smred.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/logos/smred.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/logos/smred.png',
         type: 'image/svg+xml',
       },
     ],
